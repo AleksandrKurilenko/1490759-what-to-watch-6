2f2074f0fc763a52493b84de2dfe2e87f39c2708
AleksandrKurilenko <kurilchnaprimer@gmail.com>
2021-02-28 18:51:47 +0300

    mocks

--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -1,18 +1,24 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
+import {useHistory} from 'react-router-dom';
 
-const Player = ({duration}) => {
+const Player = ({duration, title}) => {
   const [timeStamp] = useState({
     hour: Math.trunc(duration / 60),
     minutes: duration % 60,
     seconds: (duration * 60) % 60
   });
 
+  const history = useHistory();
+  const handleClick = () => {
+    history.goBack();
+  };
+
   return (
     <React.Fragment>
       <div className="player">
         <video src="#" className="player__video" poster="img/player-poster.jpg" />
-        <button type="button" className="player__exit">Exit</button>
+        <button type="button" className="player__exit" onClick={handleClick}>Exit</button>
         <div className="player__controls">
           <div className="player__controls-row">
             <div className="player__time">
@@ -28,7 +34,7 @@ const Player = ({duration}) => {
               </svg>
               <span>Play</span>
             </button>
-            <div className="player__name">Transpotting</div>
+            <div className="player__name">{title}</div>
             <button type="button" className="player__full-screen">
               <svg viewBox="0 0 27 27" width={27} height={27}>
                 <use xlinkHref="#full-screen" />
@@ -44,6 +50,7 @@ const Player = ({duration}) => {
 
 Player.propTypes = {
   duration: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired
 };
 
 

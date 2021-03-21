f4a7d9f145d04d079570cd9a300344ced981ca8d
AleksandrKurilenko <kurilchnaprimer@gmail.com>
2021-03-21 23:15:04 +0300

    bug fixes with video player

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,38 +1,19 @@
-import React, {useState} from "react";
+import React from "react";
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 import VideoPreview from "../video-preview/video-preview";
 
 
 const MovieCard = (props) => {
-  const {title, poster, id, setActiveIdFilm, previewVideoLink} = props;
-
-  const [isActive, setIsActive] = useState(false);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  const onMouseEnterMovieCard = () => {
-    setIsActive(true);
-    setIsPlaying(true);
-  };
-
-  const onMouseLeaveMovieCard = () => {
-    setIsActive(false);
-  };
+  const {title, poster, id, previewVideoLink} = props;
 
   return (
-    <article className="small-movie-card catalog__movies-card"
-      onMouseEnter={() => {
-        setActiveIdFilm(id);
-        onMouseEnterMovieCard();
-      }}
-      onMouseLeave={() => onMouseLeaveMovieCard()}>
-      <div className="small-movie-card__image">
-        {isActive ? <VideoPreview poster={poster} url={previewVideoLink} isPlaying={isPlaying} setIsPlaying={setIsPlaying} /> : <img src={poster} alt={title} width={280} height={175} />}
-      </div>
+    <article className="small-movie-card catalog__movies-card">
+      <VideoPreview poster={poster} id={id} url={previewVideoLink} title = {title}/>
       <h3 className="small-movie-card__title">
         <Link className="small-movie-card__link" to={`/films/${id}`}>{title}</Link>
       </h3>
-    </article>
+    </article >
   );
 };
 
@@ -40,7 +21,6 @@ MovieCard.propTypes = {
   poster: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
-  setActiveIdFilm: PropTypes.func.isRequired,
   previewVideoLink: PropTypes.string.isRequired,
 };
 
--- src/components/video-preview/video-preview.jsx
+++ src/components/video-preview/video-preview.jsx
@@ -1,11 +1,12 @@
-import React, {createRef, useEffect} from 'react';
+import React, {createRef, useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 
 const TIMEOUT_VIDEO_PLAY = 1000;
 
-const VideoPreview = ({poster, url, isPlaying, setIsPlaying}) => {
+const VideoPreview = ({poster, title, url}) => {
   const videoRef = createRef();
-
+  const [isPlaying, setIsPlaying] = useState(false);
+  // const videoUrl = `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`;
   let timerID = null;
 
   useEffect(() => {
@@ -16,21 +17,34 @@ const VideoPreview = ({poster, url, isPlaying, setIsPlaying}) => {
     }
 
     return () => {
-      setIsPlaying(false);
       timerID = clearTimeout(timerID);
     };
   }, [isPlaying]);
 
+  const onMouseEnterMovieCard = () => {
+    setIsPlaying(true);
+  };
+
+  const onMouseLeaveMovieCard = () => {
+    setIsPlaying(false);
+  };
+
   return (
-    <video src={url} ref={videoRef} width={280} height={175} poster={poster} muted></video>
+    <div className="small-movie-card__image"
+      onMouseEnter={() => {
+        onMouseEnterMovieCard();
+      }}
+      onMouseLeave={() => onMouseLeaveMovieCard()}>
+      {isPlaying ? < video src={url} ref={videoRef} width={280} height={175} poster={poster} muted></video> : <img src={poster} alt={title} width={280} height={175} />}
+    </div>
   );
 };
 
 VideoPreview.propTypes = {
   poster: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
-  isPlaying: PropTypes.bool.isRequired,
-  setIsPlaying: PropTypes.func.isRequired
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
 };
 
 export default VideoPreview;

2923a7bf64902d569d22070e5963818453d6fecf
AleksandrKurilenko <kurilchnaprimer@gmail.com>
2021-04-01 13:05:27 +0300

    fix2

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,21 +1,25 @@
 import {ActionCreator} from "./action";
 
 const adaptToClient = (film) => {
-  const adaptedFilm = Object.assign(
-      {},
-      film,
-      {
-        posterImage: film.poster_image,
-        previeImage: film.preview_image,
-        backgroundImage: film.background_image,
-        backgroundColor: film.background_color,
-        scoresCount: film.scores_count,
-        runTime: film.run_time,
-        isFavorite: film.is_favorite,
-        videoLink: film.video_link,
-        previewVideoLink: film.preview_video_link
-      }
-  );
+  const adaptedFilm = {
+    name: film.name,
+    posterImage: film.poster_image,
+    previeImage: film.preview_image,
+    backgroundImage: film.background_image,
+    backgroundColor: film.background_color,
+    description: film.description,
+    rating: film.rating,
+    scoresCount: film.scores_count,
+    director: film.director,
+    starring: film.starring,
+    runTime: film.run_time,
+    genre: film.genre,
+    released: film.released,
+    id: film.id,
+    isFavorite: film.is_favorite,
+    videoLink: film.video_link,
+    previewVideoLink: film.preview_video_link
+  };
 
   return adaptedFilm;
 };
